5695183edd43922f7f12121ff17b7d135fcc59df
zubair <zubair@zubairs-MacBook-Pro.local>
2024-04-02 16:03:08 +0500

    added animated cursor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Loader from "@/components/Loader";
+import AnimatedCursor from "react-animated-cursor";
 import "../styles/globals.css";
+import { primary_color } from "@/constants";
 
 export default function App({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
@@ -15,6 +17,53 @@ export default function App({ Component, pageProps }) {
 
   return (
     <React.Fragment>
+      <AnimatedCursor
+        innerSize={14}
+        outerSize={14}
+        outerAlpha={1}
+        innerScale={1}
+        outerScale={6}
+        hasBlendMode={true}
+        showSystemCursor={true}
+        color="12, 239, 232"
+        outerStyle={{
+          mixBlendMode: "exclusion",
+        }}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+          mixBlendMode: "exclusion",
+        }}
+        // clickables={[
+        //   "a",
+        //   'input[type="text"]',
+        //   'input[type="email"]',
+        //   'input[type="number"]',
+        //   'input[type="submit"]',
+        //   'input[type="image"]',
+        //   "label[for]",
+        //   "select",
+        //   "textarea",
+        //   "button",
+        //   ".link",
+        // ]}
+      />
+      {/* <AnimatedCursor
+        color="12, 239, 232"
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={1}
+        hasBlendMode={true}
+        outerStyle={{
+          mixBlendMode: "exclusion",
+        }}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+          mixBlendMode: "exclusion",
+        }}
+      /> */}
+
       {loading ? <Loader /> : <Component {...pageProps} />}
     </React.Fragment>
   );
